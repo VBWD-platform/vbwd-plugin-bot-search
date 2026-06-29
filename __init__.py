9e189e2142738dbb16f3bfe94e8eb8237b256db1
dantweb <daniil.tkachev@gmail.com>
2026-06-29 09:08:46 +0200

    post release updates

--- __init__.py
+++ __init__.py
@@ -73,7 +73,7 @@ class BotSearchPlugin(BasePlugin):
     def metadata(self) -> PluginMetadata:
         return PluginMetadata(
             name="bot-search",
-            version="1.0.0",
+            version="26.6",
             author="VBWD Team",
             description=(
                 "A /search command that finds catalog entities (shop products, "
